a73651b6bfa2a5c7f05bc97fd0c2d9d4cc5c83ad
jcgasche <jc.gasche@mac.com>
2014-10-14 01:00:46 -0700

    update list folowed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -156,9 +156,13 @@ class UsersController < ApplicationController
 		puts user.followed_companies.inspect
 
 		user.followed_companies.each do |followed_company|
-			company = {name: followed_company.name, id: followed_company.id, 
-				logo_url: followed_company.logo_url, product_desc: followed_company.product_desc, 
-				users_following: followed_company.followers.count, total_views: followed_company.users.count}
+			company = { name: followed_company.name, id: followed_company.id, 
+				logo_url: followed_company.logo_url, 
+				high_concept: followed_company.high_concept, 
+				markets: followed_company.markets
+				users_following: followed_company.followers.count, 
+				total_views: followed_company.users.count
+			}
 			@response[:companies].push(company)
 		end
 
